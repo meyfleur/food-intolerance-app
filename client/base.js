be2c69711348b0a15af4d84a87c699d65dbce100
My Trang Hong <mytranghong@Mys-MacBook-Pro.local>
2016-10-23 22:05:43 +0200

    add  packages

--- client/base.js
+++ client/base.js
@@ -1,9 +1,8 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+import '../imports/ui/layout.js'
+
+/*Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
@@ -19,4 +18,4 @@ Template.hello.events({
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
-});
+});*/
---
+++ imports/ui/layout.js
@@ -0,0 +1,4 @@
+import {Template} from 'meteor/templating'
+import './layout.html'
+import './_includes/navigation.html'
+import './_includes/footer.html'
