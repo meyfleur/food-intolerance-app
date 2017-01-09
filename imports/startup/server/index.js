f755215fe3d9781d4b7c8faf4a20d9d72f22b707
My Trang Hong <mytrang.hong@kreuzwerker.de>
2017-01-09 23:01:56 +0100

    csv to json
    convert .txt to csv
    use papaparse to convert cvs to json
    writeFile with fs

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,9 +1,28 @@
 import { Meteor } from 'meteor/meteor';
+let fs = require('fs');
 
 Meteor.startup(() => {
+
     process.env.MAIL_URL
 
     Accounts.urls.resetPassword = function(token) {
-        return Meteor.absoluteUrl('reset-password/' + token);
+        return Meteor.absoluteUrl('reset-password/' + token)
     };
+
+    let path = process.env["PWD"] + "/imports/api/data/"
+    // let file = Assets.getText('FOOD_DES.txt').replace(/\-|  +|^\||\|$/gm,'').replace(/\r\n/,'')
+    //
+    // let foodData = Papa.parse(file, {
+    //    delimiter: '|',
+    //    header: true,
+    //    skipEmptyLines: true,
+    //    complete: function(results) {
+    //      const {data} = results
+    //      let test = JSON.stringify(data,null,2)
+    //       fs.writeFile(path+'food.json', test,(err)=>{
+    //         if(err) throw err
+    //         console.log('done')
+    //       })
+    //    }
+    // })
 });
