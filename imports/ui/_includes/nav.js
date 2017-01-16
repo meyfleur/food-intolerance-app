170d44314978cbeba4c9a58664bac1b016f6f859
My Trang Hong <mytrang.hong@kreuzwerker.de>
2017-01-16 20:29:28 +0100

    prevent keyboard mobile

--- imports/ui/_includes/nav.js
+++ imports/ui/_includes/nav.js
@@ -1,13 +1,7 @@
 import './nav.html'
 
-Template.nav.onRendered(function(){
-  // $('.button-collapse').sideNav({
-  //    closeOnClick: true
-  // });
-});
-
 Template.nav.events({
-  "click .button-collapse": function(event, template){
+  "click .button-collapse"(){
     $('.button-collapse').sideNav({
       closeOnClick: true,
       menuWidth: 170
