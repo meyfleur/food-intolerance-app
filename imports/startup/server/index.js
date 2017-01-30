5c4ca71bac6adf54d8239edaa48ee79c487135c6
My Trang Hong <mytrang.hong@kreuzwerker.de>
2017-01-30 13:57:17 +0100

    Revert "change nav, ENV"
    This reverts commit 2ba3369044e310bac4438381240d52ba8a178c6c.

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -3,7 +3,7 @@ let fs = require('fs');
 
 Meteor.startup(() => {
 
-    MAIL_URL = process.env.MAIL_URL
+    process.env.MAIL_URL
 
     Accounts.urls.resetPassword = function(token) {
         return Meteor.absoluteUrl('reset-password/' + token)
